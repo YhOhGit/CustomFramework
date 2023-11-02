634c7c39aa671abbff22ad51158bf9c1fa716f1f
YoungHo <youngho.oh@geoplan.kr>
2023-11-02 15:30:06 +0900

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by mon2yhoh on 2023/11/02.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work ... ")
+    }
+}
